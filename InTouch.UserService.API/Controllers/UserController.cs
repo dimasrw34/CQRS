908c22e35b0c2f55dfdc607b7c82dfc5e6871a87
dimasrw34 <boyarskidm@yandex.ru>
2024-11-01 21:35:29 +0300

    API added

---
+++ InTouch.UserService.API/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+using InTouch.Application;
+using InTouch.UserService.Extensions;
+using InTouch.UserService.Models;
+using InTouch.UserService.Query;
+using Microsoft.AspNetCore.Http;
+
+namespace InTouch.UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController (IMediator mediator) : ControllerBase
+    {
+        [HttpPost]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResponse<CreatedUserResponse>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Create([FromBody][Required] CreateUserCommand command) =>
+            (await mediator.Send(command)).ToActionResult();
+
+        [HttpPut]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update([FromBody] [Required] UpdateUserCommand command) =>
+            (await mediator.Send(command)).ToActionResult();
+        
+        [HttpDelete("{id:guid}")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete([Required] Guid id) =>
+            (await mediator.Send(new DeleteUserCommand(id))).ToActionResult();
+        
+        /// <summary>
+        /// Gets a list of all users.
+        /// </summary>
+        /// <response code="200">Returns the list of users.</response>
+        /// <response code="500">When an unexpected internal error occurs on the server.</response>
+        [HttpGet]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(typeof(ApiResponse<IEnumerable<UserQueryModel>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll() =>
+            (await mediator.Send(new GetAllUserQuery())).ToActionResult();
+    }
+}
--- InTouch.UserService.API/Program.cs
+++ InTouch.UserService.API/Program.cs
@@ -1,15 +1,44 @@
+using System.Globalization;
+using FluentValidation;
+using FluentValidation.Resources;
+using InTouch.Application;
+using InTouch.Infrastructure;
+using InTouch.UserService;
+using InTouch.UserService.Core;
+using InTouch.UserService.Extensions;
+using InTouch.UserService.Query;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.Configure<JsonOptions>(jsonOptions => jsonOptions.JsonSerializerOptions.Configure());
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddControllers()
+    .AddJsonOptions(_ => { });
+
+builder.Services.ConfigureAppSettings();
+builder.Services.AddRegisterTypeHandler();
+builder.Services.AddInfrastructure();
+builder.Services.AddCommandHandlers();
+builder.Services.AddQueryHandlers();
+builder.Services.AddWriteOnlyRepositories();
+builder.Services.AddResponseMediatr();
+builder.Services.AddReadDbContext();
+builder.Services.AddReadOnlyRepositories();
+builder.Services.AddCacheService(builder.Configuration);
+
+
+
+// FluentValidation global configuration.
+ValidatorOptions.Global.DisplayNameResolver = (_, member, _) => member?.Name;
+ValidatorOptions.Global.LanguageManager = new LanguageManager { Enabled = true, Culture = new CultureInfo("en-US") };
+
 
 var app = builder.Build();
 
@@ -19,11 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseErrorHandling();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
