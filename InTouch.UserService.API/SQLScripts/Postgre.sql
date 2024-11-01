b15c47424c21a70493be20320b9f800bcb77b65d
dimasrw34 <boyarskidm@yandex.ru>
2024-11-01 21:49:25 +0300

    Added script file

---
+++ InTouch.UserService.API/SQLScripts/Postgre.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE users (
+                       id uuid NOT NULL,
+                       email varchar(50) NOT NULL,
+                       "password" varchar(128) NOT NULL,
+                       "name" varchar(50) NULL,
+                       surname varchar(50) NULL,
+                       phone varchar(15) NULL,
+                       CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+
+CREATE TABLE eventstores (
+                             id uuid NOT NULL,
+                             datastamp varchar NULL,
+                             messagetype varchar NULL,
+                             aggregateid uuid NULL,
+                             createdat timestamp NULL,
+                             CONSTRAINT "EventStores_pk" PRIMARY KEY (id)
+);
